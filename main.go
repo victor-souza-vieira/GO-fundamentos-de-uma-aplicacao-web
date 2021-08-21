920f9ef6202f70eee161463a21c6159c549a21fb
victor souza <victor.ti.souza@gmail.com>
2021-08-21 17:29:54 -0300

    Dados dinamicos

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Bem maneira fi", Preco: 150, Quantidade: 10},
+		{"Notebook", "Brabo", 2500, 2},
+		{"Headset", "Redragon", 250, 2},
+	}
+
+	templates.ExecuteTemplate(w, "Index", produtos)
 }
