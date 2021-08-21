fcd75b8011c0e76dd95b7fcbe158825e6c108994
victor souza <victor.ti.souza@gmail.com>
2021-08-21 19:35:41 -0300

    Conexao com banco de dados

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
 	Quantidade int
+}
+
+func conectaComBancoDeDados() *sql.DB {
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/LojinhaGO")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
 }
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
@@ -21,11 +35,34 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Bem maneira fi", Preco: 150, Quantidade: 10},
-		{"Notebook", "Brabo", 2500, 2},
-		{"Headset", "Redragon", 250, 2},
+	db := conectaComBancoDeDados()
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produto := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produto.Nome = nome
+		produto.Descricao = descricao
+		produto.Preco = preco
+		produto.Quantidade = quantidade
+
+		produtos = append(produtos, produto)
 	}
 
 	templates.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
