3ca37d93a07c477ae1d20f5c5d80ca582215a77a
victor souza <victor.ti.souza@gmail.com>
2021-08-22 18:48:30 -0300

    Adicionando tela de edicao

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -51,3 +51,7 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/", returnStatus)
 }
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "Edit", nil)
+}
